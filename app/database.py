c06a121a2d6ccbad13f4979af169cf72c26effdc
Dom Urso <duom22@gmail.com>
2025-07-17 23:47:57 -0400

    having an issue with database checks

--- app/database.py
+++ app/database.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/fastapi_db")
+DB_HOST = os.getenv("DB_HOST", "db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", f"postgresql://postgres:postgres@{DB_HOST}:5432/fastapi_db")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
